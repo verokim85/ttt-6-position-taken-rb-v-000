9f458fa15de9f399858d168967a4cb1f927fb559
Veronica Kim <vero.a.kim@gmail.com>
2017-12-21 01:29:30 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,7 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  board[index] == '" "' or board[index] == '""'
+  if board[index] == '" "' || board[index] == '""'
+    return false
 
 end
